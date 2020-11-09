482ef13430f3dff1f12505ef1b74ecbc1dba957a
MD Raza <raza231198@gmail.com>
2020-11-09 16:38:02 +0530

    I01WD: Bringup PitchBlack

--- omni_I01WD.mk
+++ omni_I01WD.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := I01WD
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/I01WD/device.mk)
